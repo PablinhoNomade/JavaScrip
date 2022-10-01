3214afeb1f983af4593efa6b251417605a4bdb78
pablinhonomade <pablinhonomade@gmail.com>
2022-10-01 08:34:43 -0300

    atulizadoBuscaPeloEndereco

--- Map/Homem_No_Deserto.js
+++ Map/Homem_No_Deserto.js
@@ -1,15 +1,30 @@
 const localizar = {
     latELong: [
         {
-            Latitude: -29.526056,
+            latitude: -29.526056,
             longitude: 137.463883,
+        }],
+    endereco: [
+        {
+            pais: "Estados Unidos",
+            estado: "NV",
+            cidade: "Metropolis-Ghost-Town",
+            rua: "Wells",
+            numero: "89835"
         }],
 
     BuscarALatELong: function () {
         for (item of this.latELong) {
-            return `https://www.google.com/maps/search/${item.Latitude},${item.longitude}`// link do google maps 
+            return `https://www.google.com/maps/search/${item.latitude},${item.longitude}`;// link do google maps 
+        }
+    },
+    BuscarEndereco: function () {
+        for (item of this.endereco) {
+            return `https://www.google.com/maps/search/R.+${item.rua},+${item.numero}+-+${item.cidade}+-+${item.estado}`;// link do google maps 
         }
     }
-}
+};
 
-console.log(localizar.BuscarALatELong())
+console.log(localizar.BuscarALatELong());
+
+console.log(localizar.BuscarEndereco());
