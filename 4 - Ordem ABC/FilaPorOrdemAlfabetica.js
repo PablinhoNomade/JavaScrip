b1c2293cd9e5c3c0e60116c03a75b4e008ed0c34
pablinhonomade <pablinhonomade@gmail.com>
2022-10-03 08:13:07 -0300

    03/out/2022

---
+++ 4 - Ordem ABC/FilaPorOrdemAlfabetica.js
@@ -0,0 +1,20 @@
+const pessoas = ["Breno", "David", "Adele", "Carlos"];
+
+for (i of pessoas) {
+    if (i[0] === "A") {
+        console.log(i)
+    };
+};
+for (i of pessoas) {
+    if (i[0] === "B") {
+        console.log(i)
+    };
+}; for (i of pessoas) {
+    if (i[0] === "C") {
+        console.log(i)
+    };
+}; for (i of pessoas) {
+    if (i[0] === "D") {
+        console.log(i)
+    };
+};
---
+++ 4 - Ordem ABC/README.md
@@ -0,0 +1,18 @@
+![](https://www.devmedia.com.br/arquivos/cursos/hello_world_js_2332/curso_hello_world_js_2332.jpg)
+
+#Calma ; )
+
+## Já já tem mais 
+ Enquanto isto você pode visualizar o 
+ 
+ *1- Cubos + Ifood 
+ *2- Maps 
+ *3- ContagemProgressiva&Regressiva
+
+Lembre-se o seu futuro só depende de você.
+
+---
+
+
+
+###### tags: `lógica` `nodeJS`
